245652535268397cca24fda8a396c694034f2367
ask1rlya <runifjt@gmail.com>
2026-04-18 16:28:03 +0300

    Delete device/xiaomi/fog directory

--- device/xiaomi/fog/Android.mk
+++
@@ -1,5 +0,0 @@
-# Android makefile for fog
-LOCAL_PATH := $(call my-dir)
-ifeq ($(TARGET_DEVICE),fog)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-endif
--- device/xiaomi/fog/AndroidProducts.mk
+++
@@ -1,6 +0,0 @@
-# Android Products for Linute fog
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_fog.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_fog-eng
